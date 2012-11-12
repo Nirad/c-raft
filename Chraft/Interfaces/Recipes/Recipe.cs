abbe1328faef5401bd135376f9e89099f0bce2d0
Nirad <miljours@gmail.com>
2012-11-12 14:25:50 -0500

    Re init ingredients array whit corect size fix a crafting bug...

--- Chraft/Interfaces/Recipes/Recipe.cs
+++ Chraft/Interfaces/Recipes/Recipe.cs
@@ -236,9 +236,13 @@ namespace Chraft.Interfaces.Recipes
                 // Loop through the row elements
                 foreach (XElement r in rows)
                 {
+
                     // Define variables
                     string value = r.Value;
                     string[] items = value.Split(',');
+
+                    // ToDo Better way ... ReInitialize the ingredients array this thig fix a bug....
+                    ingredients = new ItemInventory[rowCount, items.Length];
 
                     // Loop through the items
                     for (int i = 0; i < items.Length; i++)
